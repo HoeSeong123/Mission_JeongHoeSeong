3f6d041aa7e3819f98cfccd3a53ef0e858bc7138
HoeSeong123 <ghltjd3@gmail.com>
2023-04-06 19:18:04 +0900

    refactor: 호감목록 삭제 시 소유권을 확인할 때 username이 아닌 id로 확인하도록 변경

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -69,7 +69,7 @@ public class LikeablePersonController {
         LikeablePerson likeablePerson = likeablePersonService.findById(id);
 
         //소유권이 본인에게 있는지 체크
-        if (!likeablePerson.getFromInstaMemberUsername().equals(rq.getMember().getInstaMember().getUsername())) {
+        if (likeablePerson.getFromInstaMember().getId() != rq.getMember().getInstaMember().getId()) {
             return rq.historyBack("해당 항목에 대한 소유권이 없습니다.");
         }
 
