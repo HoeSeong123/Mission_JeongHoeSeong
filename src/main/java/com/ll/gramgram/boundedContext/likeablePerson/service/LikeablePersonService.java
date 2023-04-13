f7a83dc0394a64a3ea16527fc00e9cc165ac6765
HoeSeong123 <ghltjd3@gmail.com>
2023-04-13 15:22:51 +0900

    refactor: 불필요한 setter 및 save 제거

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -41,10 +41,7 @@ public class LikeablePersonService {
                 return RsData.of("F-3", "이미 등록된 호감상대입니다.");
             } else {
                 String originAttractiveTypeDisplayName = likeablePerson.getAttractiveTypeDisplayName();     //기존 유형 저장
-
-                likeablePerson.setModifyDate(LocalDateTime.now());
                 likeablePerson.setAttractiveTypeCode(attractiveTypeCode);
-                likeablePersonRepository.save(likeablePerson);
 
                 return RsData.of("S-2", "%s에 대한 호감사유를 %s에서 %s(으)로 변경합니다.".formatted(username, originAttractiveTypeDisplayName, likeablePerson.getAttractiveTypeDisplayName()));
             }
