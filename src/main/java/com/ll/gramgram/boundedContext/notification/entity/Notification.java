4a173c889269ad719c713383016595be4f3dbeac
HoeSeong123 <ghltjd3@gmail.com>
2023-05-02 15:23:13 +0900

    feat: 호감표시를 받은 경우 알림페이지에 나타나도록 구현

--- src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/entity/Notification.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -24,9 +25,49 @@ public class Notification extends BaseEntity {
     @ManyToOne
     @ToString.Exclude
     private InstaMember fromInstaMember; // 메세지를 발생시킨 행위를 한 사람(호감표시한 사람)
-    private String typeCode; // 호감표시=Like, 호감사유변경=ModifyAttractiveType, 
+    private String typeCode; // 호감표시=Like, 호감사유변경=ModifyAttractiveType,
     private String oldGender; // 해당사항 없으면 null
     private int oldAttractiveTypeCode; // 해당사항 없으면 0
     private String newGender; // 해당사항 없으면 null
     private int newAttractiveTypeCode; // 해당사항 없으면 0
+
+    public String getElapsedTimeStrHuman() {
+        Duration duration = Duration.between(getCreateDate(), LocalDateTime.now());
+        long hour = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+
+        if(hour <= 0 && minutes <= 0) return "방금 전";
+        else if(hour <= 0 && minutes > 0) return "%s분 전".formatted(minutes);
+        else return "%s시간 전".formatted(hour);
+    }
+
+    public String getOldAttractiveTypeDisplayName() {
+        return switch (oldAttractiveTypeCode) {
+            case 1 -> "외모";
+            case 2 -> "성격";
+            default -> "능력";
+        };
+    }
+
+    public String getNewAttractiveTypeDisplayName() {
+        return switch (newAttractiveTypeCode) {
+            case 1 -> "외모";
+            case 2 -> "성격";
+            default -> "능력";
+        };
+    }
+
+    public String getOldGenderDisplayName() {
+        return switch (oldGender) {
+            case "W" -> "여성";
+            default -> "남성";
+        };
+    }
+
+    public String getNewGenderDisplayName() {
+        return switch (newGender) {
+            case "W" -> "여성";
+            default -> "남성";
+        };
+    }
 }
